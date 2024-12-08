a3408e46d122b1484d80867d18fd9878fde9b905
MatLamber <47279447+MatLamber@users.noreply.github.com>
2024-12-08 08:59:54 -0300

    Actualizado el comportamiento de la bomba

--- Assets/_KingPin/Scripts/LootBombController.cs
+++ Assets/_KingPin/Scripts/LootBombController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using MoreMountains.Feedbacks;
 using UnityEngine;
@@ -10,28 +10,59 @@ public class LootBombController : MonoBehaviour
     [SerializeField] private GameObject bomb;
     [SerializeField] private GameObject explosion;
     [SerializeField] private ParticleSystem explosionParticle;
-    private Vector3 originalScale;
 
+    [SerializeField] private float graceTime = 3.0f; // Tiempo de gracia después de registrar los colliders iniciales
 
+    private HashSet<Collider> initialColliders = new HashSet<Collider>();
+    private bool isIgniteMode = false;
+    private bool checkForNewCollisions = false;
+    private float elapsedTime = 0.0f;
 
     private void Start()
     {
-        originalScale = transform.localScale;
+        RegisterInitialCollisions();
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= graceTime && !checkForNewCollisions)
+        {
+            checkForNewCollisions = true; // Activar la verificación de nuevas colisiones después del tiempo de gracia
+            Debug.Log("Tiempo de gracia terminado. Listo para verificar nuevas colisiones.");
+        }
+    }
+
+    private void RegisterInitialCollisions()
+    {
+        Collider[]
+            colliders = Physics.OverlapSphere(transform.position, 0.5f); // Asume un radio pequeño alrededor del objeto
+        foreach (Collider collider in colliders)
+        {
+            initialColliders.Add(collider);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (checkForNewCollisions && !initialColliders.Contains(collision.collider) && !isIgniteMode)
+        {
+            OnIgnite();
+        }
     }
 
     private void OnIgnite()
     {
+        isIgniteMode = true;
+        Debug.Log("La bomba estalla.");
         igniteFeedBack?.PlayFeedbacks();
-        transform.DOScale(originalScale * scaleMultiplier, 2f).OnComplete(() =>
+        transform.DOScale(transform.localScale * scaleMultiplier, 2f).OnComplete(() =>
         {
             bomb.SetActive(false);
             explosionParticle.Play();
             explosion.SetActive(true);
         });
+        // Opcional: Desactiva el script o realiza otras acciones post-explosión
     }
-    
-    
-    
-
-    
 }
